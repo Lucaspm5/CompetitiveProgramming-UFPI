75ad11e8b674fe7d4683c1dff7012981e3e1f6f0
Lucas Emanuel <125845662+Lucaspm5@users.noreply.github.com>
2023-05-07 16:14:30 -0300

    Update 1110.cpp

--- Listas/2./1110.cpp
+++ Listas/2./1110.cpp
@@ -2,21 +2,36 @@
 
 using namespace std;
 
-int main()
+int main() 
 {
     int N;
 
-    while(N != 0)
+    while(cin >> N && N != 0) 
     {
-
-        cin >> N;
+        queue<int> cartas;
 
         if(N <= 50)
         {
-            
-        }else{
-            break;
-        }
+            for (int i = 1; i <= N; i++)
+                cartas.push(i);
+
+            cout << "Discarded cards:";
+
+            while(cartas.size() > 1) 
+            {
+                cout << " " << cartas.front();
+
+                cartas.pop();
+                cartas.push(cartas.front());
+                cartas.pop();
+
+                if(cartas.size() > 1) cout << ","; 
+            }
+
+            cout << endl;
+            cout << "Remaining card: " << cartas.front() << endl;
+        }else break;
     }
+
     return 0;
 }
